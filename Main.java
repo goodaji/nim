fe225d13f5b60048a47c6fdfe65d95ebe999a169
goodaji <32647617+goodaji@users.noreply.github.com>
2024-12-06 21:36:22 +0000

    Added custom starting board to Main class

--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Nim!");
 
-        Game nim = new Game();
+        int[] startingBoard = new int[] { 1, 3, 5, 7, 9 };
+        int[] players = new int[] { 0, 0 };
+        boolean interactive = true;
+
+        Game nim = new Game(startingBoard, interactive, players);
         nim.playGame();
         nim.printWinner();
     }
